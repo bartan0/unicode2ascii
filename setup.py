ef3cab50ce055693dd46fbcd30f769136acade3e
Bartłomiej Sługocki <0@bartan0.pl>
2018-04-04 20:42:49 +0200

    metainformation, better file structure of the package
    Added files LICENSE, MANIFEST.in; added some package metainformation to
    setup.py; the structure of the package is now good in sense of Python package.

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+def file_read(fname):
+	with open(fname) as f:
+		res = f.read()
+	return res
 
 setup(
 	name = 'unicode2ascii',
-	version = '0.1',
-	packages = find_packages(),
+	version = '0.1.1',
+	license = 'MIT',
+	packages = ['unicode2ascii', 'unicode2ascii.tests'],
+	url = 'https://github.com/bartan0/unicode2ascii',
+
+	description = \
+		'Small and simple package to convert Unicode strings' \
+		' to their ASCII counterparts.',
+	long_description = file_read('README.md'),
+	keywords = 'unicode ascii utf8 utf-8 diacritics string',
 
 	author = 'Bartłomiej Sługocki',
-	author_email = '0@bartan0.pl'
+	author_email = '0@bartan0.pl',
 )
